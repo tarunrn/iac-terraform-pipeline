a42f4b072b624f92f51c09c831e46b2896d9615e
tarunrn <tarunreddy163@gmail.com>
2026-03-04 17:52:43 -0500

    feat: add Terraform IaC pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
+    iam = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "app_bucket" {
+  bucket        = "my-app-bucket-dev"
+  force_destroy = true
+  tags          = { Environment = "dev", Project = "iac-pipeline" }
+}
+
+resource "aws_iam_role" "app_role" {
+  name = "app-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
